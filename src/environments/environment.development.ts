04d71d70f15e4a38c0bdf8cbcd6484890a59fd7a
David Gonzales <david25bmx.dg@gmail.com>
2025-09-15 14:56:17 -0500

    fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // endpoint: "https://grupohoreb.azurewebsites.net/api/v1/"
-  endpoint: "backendsistemaventa-production.up.railway.app/api/v1/"
+  endpoint: "https://backendsistemaventa-production.up.railway.app/api/v1/"
   //endpoint: "https://localhost:7119/api/v1/"
 };
